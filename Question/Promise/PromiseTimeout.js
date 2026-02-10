5b71452185dbda3ecd784259971ba4a5ed7b15bd
Viwek Ranjan <viwekranjan@Viweks-MacBook-Pro.local>
2026-02-10 23:47:20 +0530

    Promise Timeout: Reject a promise if it takes too long.

---
+++ Question/Promise/PromiseTimeout.js
@@ -0,0 +1,33 @@
+// Promise Timeout: Reject a promise if it takes too long. 
+const timeout = 2000; // 2 seconds
+
+const apiCall = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Data received");
+    }, 3000);
+  });
+  
+
+function PromiseTimeout(apiCall,timeout){
+  return new Promise((resolve,reject)=>{
+    const timer = setTimeout(()=> {
+        reject(new Error("Operation timed out"));
+    },timeout)
+    apiCall
+      .then((value) => {
+        clearTimeout(timer);
+        resolve(value);
+      })
+      .catch((err) => {
+        clearTimeout(timer);
+        reject(err);
+      });
+
+
+
+  })
+}
+
+PromiseTimeout(apiCall, timeout)
+  .then(console.log)
+  .catch(console.error);
